2e847fcabfbd22addffe9504cd367504f55c07cf
Tima <141612775+t1matoma@users.noreply.github.com>
2025-04-04 13:20:39 +0600

    Update models.py

--- teplica/heating/models.py
+++ teplica/heating/models.py
@@ -8,8 +8,8 @@ class HeatingStatus(models.Model):
     ]
 
     greenhouse = models.OneToOneField(Greenhouse, on_delete=models.CASCADE)  # Привязка к теплице
-    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='off')  # Включено/выключено
-    temperature = models.IntegerField(default=20)  # Температура отопления
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='off') 
+    temperature = models.IntegerField(default=20)
 
     def __str__(self):
         return f"Отопление ({self.greenhouse.name}): {self.get_status_display()} - {self.temperature}°C"
