0cf435644447a5a5058a7f76dcfd2770c9070171
Tima <141612775+t1matoma@users.noreply.github.com>
2025-04-04 13:29:11 +0600

    Update models.py

--- teplica/steam_generator/models.py
+++ teplica/steam_generator/models.py
@@ -9,8 +9,8 @@ class Evaporation(models.Model):
 
     greenhouse = models.OneToOneField(Greenhouse, on_delete=models.CASCADE)  # Привязка к теплице
     status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='off')
-    start_time = models.TimeField(null=True, blank=True)  # Время начала
-    duration = models.IntegerField(default=30)  # Длительность в минутах
+    start_time = models.TimeField(null=True, blank=True)  
+    duration = models.IntegerField(default=30)  
 
     def __str__(self):
         return f"Пароиспарение ({self.greenhouse.name}): {self.get_status_display()} | Начало: {self.start_time} | Длительность: {self.duration} мин."
