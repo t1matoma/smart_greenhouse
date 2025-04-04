dacb6aa983f8b1b0836986140de817f3caee74bd
Tima <141612775+t1matoma@users.noreply.github.com>
2025-04-04 13:31:55 +0600

    Update models.py

--- teplica/users/models.py
+++ teplica/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Greenhouse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Привязка к пользователю
-    name = models.CharField(max_length=100)  # Название теплицы
-    created_at = models.DateTimeField(auto_now_add=True)  # Дата создания
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return f"{self.name} (Владелец: {self.user.username})"
